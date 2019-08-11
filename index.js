8d22d088f5607280bc64db811b1856d549f4a57f
vitaliytv <vitaliytv@nitralabs.com>
2019-08-11 22:33:17 +0300

    log if not found

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const whoiser = require('whoiser');
+const whoiser = require('whoiser')
+const log = require('loglevel-colored-level-prefix')()
 
 /**
  * Get domain expiration date
@@ -10,12 +11,16 @@ const whoiser = require('whoiser');
  *
  *     whoisExpire('nitra.ai')
  */
-async function whoisExpire(domain) {
-        const domainInfo = await whoiser.domain(domain)
-        const firstData = domainInfo[Object.keys(domainInfo)[0]]
-        const whooisDate = firstData['Registry Expiry Date'] || firstData['Expires'] || firstData['expires']|| firstData['paid-till'] 
-        const result = new Date(Date.parse(whooisDate))
+async function whoisExpire (domain) {
+  const domainInfo = await whoiser.domain(domain)
 
-  return result
+  const firstData = domainInfo[Object.keys(domainInfo)[0]]
+  const whooisDate = firstData['Registry Expiry Date'] || firstData['Expires'] || firstData['expires'] || firstData['paid-till']
+  if (whooisDate) {
+    log.debug(whooisDate)
+    return new Date(Date.parse(whooisDate))
+  }
+
+  log.debug(domainInfo)
 }
 module.exports = whoisExpire
--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 const whoisExpire = require('./index')
 
+/* global test, expect */
 test('Get expiration for nitra.ai', async () => {
   expect(await whoisExpire('nitra.ai')).toStrictEqual(new Date('2020-02-27T07:51:18.881Z'))
 })

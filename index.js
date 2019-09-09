d7f99fe118d26af805e675f169b55f052c7f3b86
vitaliytv <vitaliytv@nitralabs.com>
2019-09-09 15:18:25 +0300

    upgrade deps

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ async function whoisExpire (domain) {
   const domainInfo = await whoiser.domain(domain)
 
   const firstData = domainInfo[Object.keys(domainInfo)[0]]
-  const whooisDate = firstData['Registry Expiry Date'] || firstData['Expires'] || firstData['expires'] || firstData['paid-till']
+  const whooisDate = firstData['Registry Expiry Date'] || firstData.Expires || firstData.expires || firstData['paid-till']
   if (whooisDate) {
     log.debug(whooisDate)
     return new Date(Date.parse(whooisDate))
   }
   // adv24.lt
-  if (firstData['text']) {
-    for (const line of firstData['text']) {
+  if (firstData.text) {
+    for (const line of firstData.text) {
       const lineLowered = line.toLowerCase()
       if (lineLowered.startsWith('expires')) {
         const lineDate = line.substr(-10, 10)
